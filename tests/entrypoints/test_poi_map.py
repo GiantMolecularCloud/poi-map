38222c4ed94b3993fc4eddc53b7fd68727f6171e
Nico Krieger <nico.krieger@btc-ag.com>
2024-12-19 16:55:55 +0100

    add basic test

---
+++ tests/entrypoints/test_poi_map.py
@@ -0,0 +1,22 @@
+import time
+import signal
+import subprocess
+
+
+class TestEntrypoint:
+    def test_poetry_entrypoint_is_set(self):
+        process = subprocess.Popen(
+            [
+                "poetry",
+                "run",
+                "poi-map",
+                "--help",
+            ]
+        )
+
+    def test_poetry_entrypoint_runs(self):
+        process = subprocess.Popen(
+            ["poetry", "run", "poi-map", "tests/test-data/config.json"]
+        )
+        time.sleep(2)
+        process.send_signal(signal.SIGINT)
