b4338eae9d7075d59333388bad5d66474d05b19a
Nico Krieger <26749617+GiantMolecularCloud@users.noreply.github.com>
2026-01-28 18:08:02 +0100

    update major dependencies

--- poi_map/io/database.py
+++ poi_map/io/database.py
@@ -1,9 +1,8 @@
-from datetime import date
 from pathlib import Path
 
 import numpy as np
 import pandas as pd
-import pandera as pa
+import pandera.pandas as pa
 from pandera.typing import Series
 
 
